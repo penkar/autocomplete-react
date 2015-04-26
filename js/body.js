6f3e5f749efb70dbdb979593bd9247767bc9f2de
jsp <jspenkar@gmail.com>
2015-04-26 18:22:00 -0500

    Moved the components together for the time being

--- js/body.js
+++ js/body.js
@@ -1,8 +1,25 @@
+var List = React.createClass({
+	displayName: 'List',
+	render: function(){
+		return ( React.createElement('div', null, 'Things') );
+	}
+})
+
+var ListItem = React.createClass({
+	displayName: 'ListItem',
+	render: function(){
+		return ( React.createElement('div', null, 'ListItem') )
+	}
+})
+
+
+// var List = require('./components/list.js')
+
 var Body = React.createClass({
 	displayName: 'Body',
 	render: function(){
 		return(
-			React.createElement('div', {className:'body'}
+			React.createElement(List, null
 			)
 		)
 	}
---
+++ js/components/list.js
@@ -0,0 +1,11 @@
+var List = React.createClass({
+	displayName: 'List',
+	render: function(){
+		return ( React.createElement('div', null, 'Things')
+		);
+	}
+})
+
+
+
+module.exports = List
