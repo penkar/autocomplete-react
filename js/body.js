7f128b9d27d829124de031ddd11246bed2531035
jsp <jspenkar@gmail.com>
2015-04-27 16:34:12 -0500

    div list of options and click functionality working.

--- js/body.js
+++ js/body.js
@@ -1,46 +1,46 @@
 var exampleData = {
 	titles: [
-		'Mexican Black Bean Soup',
-		'Eggpland and Roasted Garlic Babakanoosh',
-		'Bathingan bel Khal Wel thome',
-		'Lebanese Tabbouleh',
-		'BabaGhanooj',
-		'Falfafel',
-		'Pita Bread',
-		'Kebab Koubane',
-		'Moroccan Yogurt Dip',
-		'Persian Cutlet',
-		'Omlet',
-		'Hommas Balil',
-		'Marghareta Pizza',
-		'Cannoli',
-		'North Italian Cannoli',
-		'Iced Florentine',
-		'Artichoke Hummus',
-		'Baked White Bean Dip with Rosemary and Parmesan',
-		'Black Bean Dip',
-		'Blood Orange and Green Olive Salad',
-		'Bruschetta Three Ways',
-		'Chili Garlic Corn Chips',
-		'Chilled Ginger Cantaloupe Soup',
-		'Chipotle Bacon Deviled Eggs',
-		'Coconut Shrimp Poppers with Chili Mango Cream',
-		'Creamy Blue Cheese Dip with Lemon and Chives',
-		'Deviled Eggs',
-		'Herbed Cheddar Parmesan Crisps',
-		"Kettle Corn",
-		'Maple-Rosemary Glazed Walnuts',
-		'Nando’s Honey Garlic Wings',
-		'Pickled Serrano Chiles',
-		'Potato Taquitos',
-		'Pumpkin Spice Cream Cheese Spread',
-		'Roasted Red Pepper Hummus',
-		'Rosemary Garlic Sweet Potato Fries',
-		'Slow-Roasted Cherry Tomatoes',
-		'Smoked Tuna Dip',
-		'Spicy Pickled Garlic',
-		'Spinach and Feta Turnovers',
-		'Sun-dried Tomato Hummus',
+		// 'Mexican Black Bean Soup',
+		// 'Eggpland and Roasted Garlic Babakanoosh',
+		// 'Bathingan bel Khal Wel thome',
+		// 'Lebanese Tabbouleh',
+		// 'BabaGhanooj',
+		// 'Falfafel',
+		// 'Pita Bread',
+		// 'Kebab Koubane',
+		// 'Moroccan Yogurt Dip',
+		// 'Persian Cutlet',
+		// 'Omlet',
+		// 'Hommas Balil',
+		// 'Marghareta Pizza',
+		// 'Cannoli',
+		// 'North Italian Cannoli',
+		// 'Iced Florentine',
+		// 'Artichoke Hummus',
+		// 'Baked White Bean Dip with Rosemary and Parmesan',
+		// 'Black Bean Dip',
+		// 'Blood Orange and Green Olive Salad',
+		// 'Bruschetta Three Ways',
+		// 'Chili Garlic Corn Chips',
+		// 'Chilled Ginger Cantaloupe Soup',
+		// 'Chipotle Bacon Deviled Eggs',
+		// 'Coconut Shrimp Poppers with Chili Mango Cream',
+		// 'Creamy Blue Cheese Dip with Lemon and Chives',
+		// 'Deviled Eggs',
+		// 'Herbed Cheddar Parmesan Crisps',
+		// "Kettle Corn",
+		// 'Maple-Rosemary Glazed Walnuts',
+		// 'Nando’s Honey Garlic Wings',
+		// 'Pickled Serrano Chiles',
+		// 'Potato Taquitos',
+		// 'Pumpkin Spice Cream Cheese Spread',
+		// 'Roasted Red Pepper Hummus',
+		// 'Rosemary Garlic Sweet Potato Fries',
+		// 'Slow-Roasted Cherry Tomatoes',
+		// 'Smoked Tuna Dip',
+		// 'Spicy Pickled Garlic',
+		// 'Spinach and Feta Turnovers',
+		// 'Sun-dried Tomato Hummus',
 		'Sweet and Spicy Chinese Five Spice Roasted Almonds',
 		'Tzatziki Sauce (cucumber yogurt dip)',
 		'Whole Wheat Pita Chips'
@@ -50,11 +50,10 @@ var exampleData = {
 var Title = React.createClass({
 	displayName: 'Title',
 	render: function(){
-		return React.createElement('div', {onClick: this._titleClick}, this.props.title)
+		return React.createElement('div', {onClick: this.titleClick}, this.props.title)
 	},
-	_titleClick: function(){
-		console.log(this.props)
-		// this.props._titleClick(this.props.title);
+	titleClick: function(){
+		this.props.titleClick(this.props.title);
 	}
 })
 
@@ -75,11 +74,10 @@ var List = React.createClass({
 
 		})
 	},
-	_titleClick: function(title){
-		console.log('title')
+	titleClick: function(title){
 		this.setState({
 			current:title
-		})
+		});
 	},
 	_onChange: function(event){
 		this.setState({
@@ -87,18 +85,20 @@ var List = React.createClass({
 		})
 	},
 	render: function(){
+		document.aaa = this;
 		var opts = [], divs = [];
 		var data = this.state.titles;
-		var current = this.state.current || '';
+		var current = this.state.current.toLowerCase() || '';
+		var titleClick = this.titleClick;
 		data.filter(function(title){
 			if(title.toLowerCase().indexOf(current) !== -1){
 				opts.push(React.createElement( ListItem, { val:title, key:title }) )
-				divs.push(React.createElement( Title, { title:title, key:title, _titleClick:this._titleClick }) );
+				divs.push(React.createElement( Title, { titleClick:titleClick, title:title }) );
 			}
 		})
 		return ( 
 			React.createElement('div', null,
-				React.createElement('input', {className: 'autoComplete', onChange: this._onChange}),
+				React.createElement('input', {value: this.state.current, className: 'autoComplete', onChange: this._onChange}),
 				React.createElement('br', null),
 				React.createElement('select', {className: 'autoComplete'}, opts),
 				divs 
@@ -115,8 +115,7 @@ var Body = React.createClass({
 	displayName: 'Body',
 	render: function(){
 		return(
-			React.createElement(List, null
-			)
+			React.createElement(List, null)
 		)
 	}
 })
